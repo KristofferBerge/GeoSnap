d42ac4aa08fc74c643288eb0a4627d9a395d1ea6
Kristoffer Berge <kristoffer.berge@gmail.com>
2016-02-03 18:15:03 +0100

    Moving location request to service

---
+++ app/src/main/java/com/example/krist/geosnap/Services/ApiCommunicator.java
@@ -0,0 +1,7 @@
+package com.example.krist.geosnap.Services;
+
+/**
+ * Created by krist on 03-Feb-16.
+ */
+public class ApiCommunicator {
+}
---
+++ app/src/main/java/com/example/krist/geosnap/Services/GeoService.java
@@ -0,0 +1,17 @@
+package com.example.krist.geosnap.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class GeoService extends Service {
+    public GeoService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
